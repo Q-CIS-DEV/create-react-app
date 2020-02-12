bd82f92366bfcedde3f527495d69b29a2d23daac
Alexander Zinchenko <az67128@gmail.com>
2020-02-12 11:59:38 +0300

    Tb 305 autobuild bo (#11)
    * TB-305 autobuild config
    
    * tb-305 script added to package.json
    
    * tb-305 fixed cooments
    
    * tb-305 fix review comments
    
    * removed ; from generated scripts
    
    * removed " and dublicates in dependsOn
    
    * Trailing comma in configs, correct spacing in model file
    
    * version upgrade

--- packages/react-scripts/scripts/initMeta.js
+++ packages/react-scripts/scripts/initMeta.js
@@ -1,17 +1,18 @@
-require("dotenv").config();
-const getMeta = require("./initMetaUtils/getMeta");
-const { ask, makeBoFolder, toCamel } = require("./initMetaUtils/common");
-const writeModelFile = require("./initMetaUtils/writeModelFile.js");
-const writeFormFile = require("./initMetaUtils/writeFormFile.js");
-const writeIndexFile = require("./initMetaUtils/writeIndexFile.js");
-const writeConfigFile = require("./initMetaUtils/writeConfigFile.js");
-const writeBoConfigFile = require("./initMetaUtils/writeBoConfigFile.js");
-
+require('dotenv').config();
+const getMeta = require('./initMetaUtils/getMeta');
+const { ask, makeBoFolder, toCamel } = require('./initMetaUtils/common');
+const writeModelFile = require('./initMetaUtils/writeModelFile.js');
+const writeFormFile = require('./initMetaUtils/writeFormFile.js');
+const writeIndexFile = require('./initMetaUtils/writeIndexFile.js');
+const writeConfigFile = require('./initMetaUtils/writeConfigFile.js');
+const writeBoConfigFile = require('./initMetaUtils/writeBoConfigFile.js');
 
 // TODO add normal parsing for host
-const url =
-  process.env.REACT_APP_DEFAULT_API_HOST.replace("/custodian/data/", "") || "";
-const defaultBoCollectionName = "TroodCoreBusinessObjects";
+const url = (process.env.REACT_APP_DEFAULT_API_HOST || '').replace(
+  '/custodian/data/',
+  ''
+);
+const defaultBoCollectionName = 'TroodCoreBusinessObjects';
 const appPath = process.cwd();
 
 async function initMeta() {
@@ -20,13 +21,13 @@ async function initMeta() {
     (await ask(
       `Business objects collection name (${defaultBoCollectionName})`
     )) || defaultBoCollectionName;
-  const boCollectionPath = appPath + "/src/businessObjects/" + boCollectionName;
+  const boCollectionPath = appPath + '/src/businessObjects/' + boCollectionName;
   makeBoFolder(boCollectionPath);
   writeConfigFile({ meta, appPath, boCollectionName });
   writeBoConfigFile({ meta, boCollectionPath, boCollectionName });
   meta.forEach(businessObject => {
     const objectName = toCamel(businessObject.name);
-    const boPath = boCollectionPath + "/" + objectName;
+    const boPath = boCollectionPath + '/' + objectName;
     makeBoFolder(boPath);
     writeModelFile({ businessObject, boPath });
     writeFormFile({ businessObject, boPath });
--- packages/react-scripts/scripts/initMetaUtils/common.js
+++ packages/react-scripts/scripts/initMetaUtils/common.js
@@ -1,6 +1,7 @@
-const readline = require("readline");
-const { Writable } = require("stream");
-const fs = require("fs");
+const readline = require('readline');
+const { Writable } = require('stream');
+const fs = require('fs');
+const util = require('util');
 
 const ask = (question, muted = false) =>
   new Promise(resolve => {
@@ -8,32 +9,43 @@ const ask = (question, muted = false) =>
       write: function(chunk, encoding, callback) {
         if (!this.muted) process.stdout.write(chunk, encoding);
         callback();
-      }
+      },
     });
     mutableStdout.muted = false;
     const rl = readline.createInterface({
       input: process.stdin,
       output: mutableStdout,
-      terminal: true
+      terminal: true,
     });
-    rl.question(question + ":", function(answer) {
+    rl.question(question + ':', function(answer) {
       rl.close();
       // To add line after password
-      if (muted) console.log("");
+      if (muted) console.log('');
       resolve(answer);
     });
     mutableStdout.muted = muted;
   });
 
 function toCamel(string) {
-  return string.replace(/(.)_+(.)/gi, (_, $1, $2) => $1 + $2.toUpperCase())
+  return string.replace(/(.)_+(.)/gi, (_, $1, $2) => $1 + $2.toUpperCase());
 }
 
-
 function makeBoFolder(path) {
-    fs.mkdirSync(path, { recursive: true }, err => {
-      throw new Error("Not possible to create path " + path);
-    });
-  }
+  fs.mkdirSync(path, { recursive: true }, err => {
+    throw new Error('Not possible to create path ' + path);
+  });
+}
 
-module.exports = { ask, toCamel, makeBoFolder };
+function addTrailingComma(string) {
+  return string.replace(/(\w|}|')(\r\n|\r|\n)/gi, (_, $1, $2) => $1 + ',' + $2);
+}
+
+function parseObject(obj) {
+  return addTrailingComma(
+    util.inspect(obj, {
+      depth: null,
+      breakLength: 1,
+    })
+  );
+}
+module.exports = { ask, toCamel, makeBoFolder, addTrailingComma, parseObject };
