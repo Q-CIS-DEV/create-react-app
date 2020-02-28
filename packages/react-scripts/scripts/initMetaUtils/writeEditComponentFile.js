00c4bc96e9edc2be4abac7265a01e2e00a91ccb5
az67128 <a.zinchenko@atol.ru>
2020-02-28 17:53:18 +0300

    generate component

--- packages/react-scripts/scripts/initMetaUtils/writeEditComponentFile.js
+++ packages/react-scripts/scripts/initMetaUtils/writeEditComponentFile.js
@@ -1,21 +1,79 @@
 const fs = require('fs');
 const { toCamel } = require('./common');
 
+const generateProps = field => {
+  const name = toCamel(field.name);
+  return `value: model.${name},
+          errors: modelErrors.${name},
+          onChange: val => modelFormActions.changeField('${name}', val),
+          onValid: () => modelFormActions.resetFieldError('${name}'),
+          onInvalid: err => modelFormActions.setFieldError('${name}', err),
+          validate: {
+            checkOnBlur: true,
+            required: ${!field.optional},
+          },`;
+};
+
 function writeEditComponentFile({ businessObject, boPath }) {
   const objectName = toCamel(businessObject.name);
   const filePath = boPath + '/editComponent.js';
   // if (fs.existsSync(filePath)) return;
-    const generateField = (field) =>{
-        return field.name + field.type
+
+  const getImports = field => {
+    if (field.type === 'number')
+      return "import TInput, { INPUT_TYPES } from '$trood/components/TInput'";
+    return null;
+  };
+
+  const getCode = field => {
+    return null;
+  };
+
+  const getJsx = field => {
+    if (field.type === 'number') {
+      return `      <TInput
+          {...{
+          label: '${toCamel(field.name)}',
+          placeholder: 'Not chosen',
+          type: INPUT_TYPES.float,
+          ${generateProps(field)}
+        }}
+      />`;
     }
-  const generateEditComponent = (businessObject)=>{
-    const editComponent = "import React from 'react'\n" +
-    "const EditComponent = () => {\n" +
-    "  return <div>" + businessObject.fields.map(field => generateField(field)).join("\n") + "</div>\n" + 
-    "}\n"+
-    "export default EditComponent"
-    return editComponent
-  }
+    return field.name + '-' + field.type;
+  };
+  const generateEditComponent = businessObject => {
+    const components = businessObject.fields.reduce(
+      (memo, field) => {
+        const imports = getImports(field);
+        const code = getCode(field);
+        const jsx = getJsx(field);
+        return {
+          imports: [...memo.imports, ...(imports ? [imports] : [])],
+          code: [...memo.code, ...(code ? [code] : [])],
+          jsx: [...memo.jsx, ...(jsx ? [jsx] : [])],
+        };
+      },
+      {
+        imports: [],
+        code: [],
+        jsx: [],
+      }
+    );
+
+    return `import React from 'react';
+${components.imports.join('\n')}
+
+const EditComponent = ({ model, modelErrors, modelFormActions }) => {
+${components.code.join('\n')}
+  return (
+    <React.Fragment>
+${components.jsx.join('\n')}
+    </React.Fragment>
+  );
+};
+export default EditComponent`;
+  };
 
   fs.writeFileSync(filePath, generateEditComponent(businessObject), 'utf-8');
   console.log(`Generating ${objectName}/editComponent.js`);
